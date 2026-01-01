06078aa30d22bcf4355c35d543e41e302286b22c
alexalves011 <alexalves.work@gmail.com>
2026-01-01 16:06:09 -0300

    add projeto pl

---
+++ PremierLeague/premier-league/src/pages/tabela/index.jsx
@@ -0,0 +1,14 @@
+import Header from "../../components/Header"; // Importa o componente COMPLETO
+
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {/* Aqui você começará a fazer a sua tabela depois */}
+    </div>
+  );
+}
+
+export default App;
